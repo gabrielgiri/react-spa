67dc580492f6543725eade4889273eac4a1e891f
gabrielgiri <gabrielgiri@gmail.com>
2019-05-20 11:40:25 -0300

    Start using NavLink

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = (props) => (
     <nav>
         <h2 className="logo">{props.title}</h2>
         <ul className="nav-menu">
-            <li><Link className="nav-menu-link" to="/">Home</Link></li>
-            <li><Link className="nav-menu-link" to="/notebook">Notebooks</Link></li>
-            <li><Link className="nav-menu-link" to="/impresoras">impresoras</Link></li>
-            <li><Link className="nav-menu-link" to="/perifericos">perifericos</Link></li>
+            <li><NavLink className="nav-menu-link" exact to="/">Home</NavLink></li>
+            <li><NavLink className="nav-menu-link" exact to="/notebook">Notebooks</NavLink></li>
+            <li><NavLink className="nav-menu-link" exact to="/impresoras">impresoras</NavLink></li>
+            <li><NavLink className="nav-menu-link" exact to="/perifericos">perifericos</NavLink></li>
         </ul>
     </nav>
 );
