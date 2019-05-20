3c688a3f6cc911a7646a8ccff9420722aea7cb2d
gabrielgiri <gabrielgiri@gmail.com>
2019-05-20 16:04:06 -0300

    Added dropdown

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
+
+
+
+
+
+const Dropdown = () => {
+    const currentDropdownItems = [
+        {
+            url: '/sub1',
+            description: 'ITEM 1'
+        },
+        {
+            url: '/sub2',
+            description: 'ITEM 2'
+        },
+        {
+            url: '/sub3',
+            description: 'ITEM 3'
+        },
+    ]; 
+    const [activeItem, setActiveItem] = useState(currentDropdownItems[0]);
+
+
+    const renderDropdownItem = (item, index) => {
+        const { url, description } = item;
+
+        return (
+            <li onClick={() => handleDropdownItemClick(item)} key={index}>
+                <NavLink className="nav-menu-link" exact to={url}>
+                    {description}
+                </NavLink>
+            </li>
+        );
+    }
+    
+    const handleDropdownItemClick = (item) => {
+        setActiveItem(item);
+    }
+    
+    return (
+        <li>
+            <NavLink className="nav-menu-link dropdown" exact to={activeItem.url}>
+                <div className="dropbtn">Dropdown</div>
+                    <div class="dropdown-content">
+                        <ul>
+                            {currentDropdownItems.map( (item) => renderDropdownItem(item)) }
+                        </ul>
+                    </div>
+            </NavLink>
+        </li>
+    )
+}
+
 
 const NavBar = (props) => (
     <nav>
@@ -9,6 +62,7 @@ const NavBar = (props) => (
             <li><NavLink className="nav-menu-link" exact to="/notebook">Notebooks</NavLink></li>
             <li><NavLink className="nav-menu-link" exact to="/impresoras">impresoras</NavLink></li>
             <li><NavLink className="nav-menu-link" exact to="/perifericos">perifericos</NavLink></li>
+            <Dropdown />
         </ul>
     </nav>
 );
